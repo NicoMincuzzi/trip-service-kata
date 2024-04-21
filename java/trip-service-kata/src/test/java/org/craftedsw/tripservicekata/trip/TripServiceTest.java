3d0510e8438e5efa250dcb62a5d3ba0b6068b443
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2024-04-21 11:05:33 +0200

    [test]: You need to be logged in to see the content

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,41 @@
 package org.craftedsw.tripservicekata.trip;
 
-public class TripServiceTest {
-	
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.springframework.util.Assert.isTrue;
+
+public class TripServiceTest extends TripService {
+
+    private static final User REGISTERED_USER = new User();
+    private static final Trip TRIP = new Trip();
+    private User loggedInUser;
+
+    @Test
+    public void whenTheUserIsLoggedInThenShowTheContent() {
+        User user = new User();
+        user.addTrip(TRIP);
+        user.addTrip(TRIP);
+        user.addFriend(REGISTERED_USER);
+
+        loggedInUser = REGISTERED_USER;
+
+        List<Trip> result = new TestableTripService().getTripsByUser(user);
+
+        isTrue(result.size() == 2);
+    }
+
+    private class TestableTripService extends TripService {
+        protected User loggedUser() {
+            return loggedInUser;
+        }
+
+        protected List<Trip> userTrips(User user) {
+            return asList(TRIP, TRIP);
+        }
+    }
+
 }
