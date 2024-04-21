a84a05430b643f69e71e80019cd815c122958fd1
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2024-04-21 11:38:59 +0200

    [test]: If you are not logged in, the code throws an exception

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,16 +1,20 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.util.Assert.isTrue;
 
 public class TripServiceTest extends TripService {
 
     private static final User REGISTERED_USER = new User();
+    private static final User ANOTHER_USER = new User();
+    private static final User GUEST_USER = null;
     private static final Trip TRIP = new Trip();
     private User loggedInUser;
 
@@ -26,6 +30,28 @@ public class TripServiceTest extends TripService {
         List<Trip> result = new TestableTripService().getTripsByUser(user);
 
         isTrue(result.size() == 2);
+    }
+
+    @Test
+    public void whenTheUserIsLoggedInAndHasFriendThenCanSeeHisTrip() {
+        ANOTHER_USER.addTrip(new Trip());
+        User user = new User();
+        user.addTrip(TRIP);
+        user.addFriend(REGISTERED_USER);
+        user.addFriend(ANOTHER_USER);
+
+        loggedInUser = REGISTERED_USER;
+
+        List<Trip> result = new TestableTripService().getTripsByUser(user);
+
+        isTrue(result.size() == 2);
+    }
+
+    @Test
+    public void whenTheUserIsNotLoggedInAndHasFriendThenThrowAnException() {
+        loggedInUser = GUEST_USER;
+
+        assertThrows(UserNotLoggedInException.class, () -> new TestableTripService().getTripsByUser(new User()));
     }
 
     private class TestableTripService extends TripService {
